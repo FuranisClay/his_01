d41b64606e2dbbc7d3cd704c8b9a7d47493b7da6
HeuTab <1029175863@qq.com>
2022-07-31 19:38:08 +0800

    new file:   his/src/main/java/com/heu/his/controller/ConstantItemController.java 	new file:   his/src/main/java/com/heu/his/controller/DepartmentController.java 	new file:   his/src/main/java/com/heu/his/controller/FmeditemController.java 	new file:   his/src/main/java/com/heu/his/controller/RegistlevelController.java 	new file:   his/src/main/java/com/heu/his/controller/SchedulingController.java

---
+++ his/src/main/java/com/heu/his/controller/FmeditemController.java
@@ -0,0 +1,31 @@
+package com.heu.his.Controller;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.heu.his.Iservice.IFmeditemService;
+import com.heu.his.pojo.Fmeditem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author zhougy
+ * @create 2022-07-27 15:02
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("/fmeditem")
+public class FmeditemController {
+
+    @Autowired
+    IFmeditemService fmeditemService;
+
+//    @JsonFormat(pattern = "yyyy-MM-dd")
+    @RequestMapping("/list")
+    public List<Fmeditem> getList(){
+        return fmeditemService.getFmeditemList();
+    }
+
+}
