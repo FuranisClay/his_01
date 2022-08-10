fd23d9987ccb3d8aa390254f839c304ef257459c
HeuTab <1029175863@qq.com>
2022-08-10 17:00:18 +0800

    zgy

--- hisfront/src/router/index.js
+++ hisfront/src/router/index.js
@@ -10,14 +10,14 @@ const routes = [
     name: 'home',
     component: HomeView
   },
-  {
-    path: '/about',
-    name: 'about',
+//  {
+//    path: '/about',
+//    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
+//    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+//  },
   {
     path: '/his',
     name: 'his',
